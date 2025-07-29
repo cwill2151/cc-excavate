cec4b639214403d1beb567e95c0d16b9ce98747e
cwill2151 <cwill2151@users.noreply.github.com>
2025-07-29 04:51:47 +0300

    a

---
+++ initenv.lua
@@ -0,0 +1,7 @@
+return {
+init_env = function ()
+    local _require, _env = require("cc.require"), setmetatable({}, { __index = _ENV })
+    require, package = _require.make(_env, "/")
+    term.clear(); term.setCursorPos(1, 1)
+end
+}
